d660b5c8b6edfb9e453034fb777c5246d8a3fe45
DivineDX <chia.dexun97@gmail.com>
2021-10-11 16:40:29 +0800

    [UserProfileService] Integrate ESLint and Prettier

--- UserProfileService/controller/index.js
+++ UserProfileService/controller/index.js
@@ -3,18 +3,17 @@ const { Router } = require('express')
 const routes = Router()
 
 // Controller will contain all the User-defined Routes
-routes.get('/', (_, res) => res.send("Hello World with Express"))
-
+routes.get('/', (_, res) => res.send('Hello World with Express'))
 
 /**
  * Set the router of the Express Server
- * @param {express} app 
+ * @param {express} app
  */
 const routers = (app) => {
-    app.use('/', routes).all((req, res) => {
-        res.setHeader('content-type', 'application/json')
-        res.setHeader('Access-Control-Allow-Origin', '*')
-    })
+  app.use('/', routes).all((req, res) => {
+    res.setHeader('content-type', 'application/json')
+    res.setHeader('Access-Control-Allow-Origin', '*')
+  })
 }
 
 module.exports = routers
