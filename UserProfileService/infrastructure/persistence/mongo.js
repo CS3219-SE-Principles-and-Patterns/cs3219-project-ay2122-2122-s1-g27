b8206a300207e38d9ae2ebbf7dacf2b00546b315
DivineDX <chia.dexun97@gmail.com>
2021-10-17 23:29:48 +0800

    [DevOps] Updated DB Configs

--- UserProfileService/infrastructure/persistence/mongo.js
+++ UserProfileService/infrastructure/persistence/mongo.js
@@ -7,7 +7,29 @@ const user = require('../../domain/entities/user-entity')
  * (OUT PORT)
  * Layer that communicates directly with the Database
  */
-const dbURI = config.db.URI
+
+const constructMongoDBURI = (dbConfig) => {
+  if (
+    'DB_USER' in dbConfig &&
+    'DB_PASSWORD' in dbConfig &&
+    'DB_HOST' in dbConfig &&
+    'DB_PORT' in dbConfig &&
+    'DB_NAME' in dbConfig
+  ) {
+    const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME } = dbConfig
+    if (dbConfig.DB_USER === '' || dbConfig.DB_PASSWORD === '') {
+      // no need permisions
+      return `mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}`
+    }
+    return `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`
+  }
+  console.log('Warning, using default MongoDB URI')
+  return 'mongodb://localhost:27017/UserProfileDB'
+}
+
+const dbURI = constructMongoDBURI(config.db)
+console.log('Using MongoDB URI:', dbURI)
+
 const db = {} // object containing 'conn',
 
 if (dbURI) {
