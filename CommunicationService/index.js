073a4265827493dbe673cff55c3a431abe01fe1b
DivineDX <chia.dexun97@gmail.com>
2021-11-04 15:09:13 +0800

    [CommunicationService] Setup

--- CommunicationService/index.js
+++ CommunicationService/index.js
@@ -1,41 +1,38 @@
-const express = require("express");
-const app = express();
+const express = require('express')
+const app = express()
 
-const http = require("http");
-const server = http.createServer(app);
-const io = require("socket.io")(server, {
+const http = require('http')
+const server = http.createServer(app)
+const io = require('socket.io')(server, {
   cors: {
-    origin: "*",
+    origin: '*',
   },
-});
+})
 
-io.on("connection", (socket) => {
-  console.log(socket.id + " connected with socket server for text-chat!");
+io.on('connection', (socket) => {
+  console.log(socket.id + ' connected with socket server for text-chat!')
 
   //leave room done automatically by socket.io
-  socket.on("disconnect", () => {
-    console.log("user disconnected from text-chat");
-  });
+  socket.on('disconnect', () => {
+    console.log('user disconnected from text-chat')
+  })
 
   // create room for any 2 pairs of users
-  socket.on("room", function (data) {
-    console.log(String(data.username) + " joins room " + String(data.room));
+  socket.on('room', function (data) {
+    console.log(String(data.username) + ' joins room ' + String(data.room))
 
-    socket.join(data.room);
-    io.in(data.room).emit(
-      "message",
-      String(data.username) + " connected to the chat"
-    );
-  });
+    socket.join(data.room)
+    io.in(data.room).emit('message', String(data.username) + ' connected to the chat')
+  })
 
   // user interaction via text-chat
-  socket.on("interact", function (data) {
-    console.log(String(data.username) + ": " + String(data.message));
+  socket.on('interact', function (data) {
+    console.log(String(data.username) + ': ' + String(data.message))
 
-    socket.to(data.room).emit("message", data.username, data.message);
-  });
-});
+    socket.to(data.room).emit('message', data.username, data.message)
+  })
+})
 
 server.listen(7000, () => {
-  console.log("listening on *:7000");
-});
+  console.log('listening on *:7000')
+})
